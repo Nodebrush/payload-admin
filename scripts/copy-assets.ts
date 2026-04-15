9d7eced620131ed482b3bf34a6e38d00c9938229
Samuel Hasselblom <samuel@nodebrush.com>
2026-04-15 13:06:08 +0200

    move public folder to payload-config root

--- scripts/copy-assets.ts
+++ scripts/copy-assets.ts
@@ -3,7 +3,7 @@ import { resolve, dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
-const src = resolve(__dirname, '../../../packages/payload-config/src/assets/public')
+const src = resolve(__dirname, '../../../packages/payload-config/public')
 const dest = resolve(__dirname, '../public')
 
 mkdirSync(dest, { recursive: true })
